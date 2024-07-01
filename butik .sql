39ef9e9aa514b090941b65d3f029630d9cf9a765
imanuelsteven <160695521+imanuelsteven@users.noreply.github.com>
2024-07-01 13:59:00 +0700

    Add files via upload

---
+++ butik .sql
@@ -0,0 +1,264 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 01, 2024 at 08:58 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `butik`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `barang`
+--
+
+CREATE TABLE `barang` (
+  `kode` varchar(10) NOT NULL,
+  `nama` varchar(30) NOT NULL,
+  `harga` int(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `barang`
+--
+
+INSERT INTO `barang` (`kode`, `nama`, `harga`) VALUES
+('A001', 'Uniqlo Arism Tshirt', 299000),
+('A002', 'Uniqlo Arism Tshirt', 299000),
+('A003', 'Uniqlo Arism Tshirt', 299000),
+('A004', 'Uniqlo Arism Tshirt', 299000),
+('A005', 'Kaos Indomaret', 100000),
+('A006', 'Baju Alfamart', 10000),
+('A007', 'KAOS STEVEN', 10000),
+('a1000', 'ssas', 1000000);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `detail_transaksi`
+--
+
+CREATE TABLE `detail_transaksi` (
+  `id` int(11) NOT NULL,
+  `id_transaksi` int(11) NOT NULL,
+  `kode_barang` varchar(10) NOT NULL,
+  `nama_barang` varchar(100) NOT NULL,
+  `jumlah` int(11) NOT NULL,
+  `harga` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `detail_transaksi`
+--
+
+INSERT INTO `detail_transaksi` (`id`, `id_transaksi`, `kode_barang`, `nama_barang`, `jumlah`, `harga`) VALUES
+(1, 1, 'A005', 'Kaos Indomaret', 1, 100000),
+(2, 2, 'A001', 'Uniqlo Arism Tshirt', 1, 299000),
+(3, 2, 'A002', 'Uniqlo Arism Tshirt', 1, 299000),
+(4, 2, 'A003', 'Uniqlo Arism Tshirt', 2, 299000),
+(5, 3, 'A006', 'Baju Alfamart', 10, 10000),
+(6, 4, 'A007', 'KAOS STEVEN', 10, 10000);
+
+--
+-- Triggers `detail_transaksi`
+--
+DELIMITER $$
+CREATE TRIGGER `after_insert_detail_transaksi` AFTER INSERT ON `detail_transaksi` FOR EACH ROW BEGIN
+    UPDATE stok
+    SET stok = stok - NEW.jumlah
+    WHERE kode = NEW.kode_barang;
+END
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `login`
+--
+
+CREATE TABLE `login` (
+  `username` varchar(30) NOT NULL,
+  `password` varchar(8) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `login`
+--
+
+INSERT INTO `login` (`username`, `password`) VALUES
+('ADMIN', 'ADMIN'),
+('ester', 'ester'),
+('ezra', 'ezra'),
+('steven', 'steven');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `size`
+--
+
+CREATE TABLE `size` (
+  `kode` varchar(10) DEFAULT NULL,
+  `size` varchar(6) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `size`
+--
+
+INSERT INTO `size` (`kode`, `size`) VALUES
+('A001', 'XL'),
+('A002', 'L'),
+('A003', 'M'),
+('A004', 'S'),
+('A005', 'xl'),
+('A006', 'xl'),
+('A007', 'XL');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `stok`
+--
+
+CREATE TABLE `stok` (
+  `kode` varchar(10) DEFAULT NULL,
+  `stok` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `stok`
+--
+
+INSERT INTO `stok` (`kode`, `stok`) VALUES
+('a1000', 11),
+('A001', 93),
+('A002', 94),
+('A003', 90),
+('A004', 66),
+('A005', 11),
+('A006', 90),
+('A007', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaksi`
+--
+
+CREATE TABLE `transaksi` (
+  `ID_TRANSAKSI` int(11) NOT NULL,
+  `TOTAL_HARGA` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `transaksi`
+--
+
+INSERT INTO `transaksi` (`ID_TRANSAKSI`, `TOTAL_HARGA`) VALUES
+(1, 100000),
+(2, 1196000),
+(3, 100000),
+(4, 100000);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `barang`
+--
+ALTER TABLE `barang`
+  ADD PRIMARY KEY (`kode`);
+
+--
+-- Indexes for table `detail_transaksi`
+--
+ALTER TABLE `detail_transaksi`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_transaksi` (`id_transaksi`),
+  ADD KEY `kode_barang` (`kode_barang`);
+
+--
+-- Indexes for table `login`
+--
+ALTER TABLE `login`
+  ADD PRIMARY KEY (`username`);
+
+--
+-- Indexes for table `size`
+--
+ALTER TABLE `size`
+  ADD KEY `kode` (`kode`);
+
+--
+-- Indexes for table `stok`
+--
+ALTER TABLE `stok`
+  ADD KEY `kode` (`kode`);
+
+--
+-- Indexes for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD PRIMARY KEY (`ID_TRANSAKSI`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `detail_transaksi`
+--
+ALTER TABLE `detail_transaksi`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  MODIFY `ID_TRANSAKSI` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `detail_transaksi`
+--
+ALTER TABLE `detail_transaksi`
+  ADD CONSTRAINT `detail_transaksi_ibfk_1` FOREIGN KEY (`id_transaksi`) REFERENCES `transaksi` (`ID_TRANSAKSI`),
+  ADD CONSTRAINT `detail_transaksi_ibfk_2` FOREIGN KEY (`kode_barang`) REFERENCES `barang` (`kode`);
+
+--
+-- Constraints for table `size`
+--
+ALTER TABLE `size`
+  ADD CONSTRAINT `size_ibfk_1` FOREIGN KEY (`kode`) REFERENCES `barang` (`kode`);
+
+--
+-- Constraints for table `stok`
+--
+ALTER TABLE `stok`
+  ADD CONSTRAINT `stok_ibfk_1` FOREIGN KEY (`kode`) REFERENCES `barang` (`kode`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
